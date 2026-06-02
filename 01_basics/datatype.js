9d1e0810abf8df0dca5e70cfc4ddf708d0cfdffb
Shivam16a <shivam123hjp@gmail.com>
2026-06-02 12:55:52 +0530

    datatype

---
+++ 01_basics/datatype.js
@@ -0,0 +1,19 @@
+"use strict"; //treat all js code as newer version
+
+let name  = "shivam"
+let age = 21
+let isLoggedIn = false
+
+
+console.log(typeof age)
+/*
+number => 2 to power 53
+bignit
+string => ""
+boolean => true/false
+null => standlone value
+undefile =>
+symbol => unique
+
+object
+*/
