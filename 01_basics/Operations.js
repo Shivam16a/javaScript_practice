5b2495ce16636a82f2f514ea9bbadc5195fa758a
Shivam16a <shivam123hjp@gmail.com>
2026-06-03 13:00:27 +0530

    operations

---
+++ 01_basics/Operations.js
@@ -0,0 +1,12 @@
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2*2)
+// console.log(2/2)
+// console.log(2%2)
+
+let str1 = "shivam "
+let str2 = "kumar"
+
+let str3 = str1 + str2
+
+console.log(str3)
