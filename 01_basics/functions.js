948abaaa0f75d305d98ddb6aedc2d78ddbd63c1f
Shivam16a <shivam123hjp@gmail.com>
2026-06-07 10:59:41 +0530

    function IIFE

--- 01_basics/functions.js
+++ 01_basics/functions.js
@@ -16,3 +16,89 @@ function addTwonumbers(number1,number2){
 }
 
 addTwonumbers(1,4)
+
+
+// =================== scope ================
+
+if(true){
+    let a = 10
+    const b = 20
+    var c = 30
+}
+
+// console.log(a)
+// console.log(b)
+console.log(c)
+
+function add(num){
+    return num+1
+}
+
+add()
+
+const addTwo = function(num){
+    return num+2
+}
+
+
+addTwo();
+
+// ================== this ==============
+
+const user = {
+  username :"shivam",
+  price:999,
+  welcomeMessage : function(){
+    console.log(`${this.username}, welcome to website`)
+    console.log(this)
+  }
+}
+
+// user.welcomeMessage()
+
+// user.username = "abcd"
+// user.welcomeMessage()
+
+// console.log(this)
+
+// function chai(){
+//     console.log(this)
+// }
+
+// chai()
+
+// const chai = function(){
+//     let username = "shivam"
+//     console.log(this.username)
+// }
+
+
+const chai = ()=>{
+    let username = "shivam"
+    console.log(this)
+}
+
+
+// chai()
+
+// const addTwo2 = (num1, num2)=>{
+//     return num1 + num2
+// }
+
+// const addTwo2  = (num1,num2)=> num1 +num2
+
+// const addTwo2 = (num1, num2)=> (num1+num2)
+
+// const addTwo2 = (num1,num2)=>({username:"shivam"})
+
+// console.log(addTwo2(3,4));
+
+// ============ IIFE ===========
+
+(function chai(){
+    console.log(`hii shivam`)
+})();
+
+((name)=>{
+    console.log(`hello shivam ${name}`)
+})('bhi')
