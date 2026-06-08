72540b758c0349d33f7638622a48ed38462a57e7
Shivam16a <shivam123hjp@gmail.com>
2026-06-08 13:22:12 +0530

    switch

--- 01_basics/controlflow.js
+++ 01_basics/controlflow.js
@@ -25,16 +25,54 @@ if (score > 100) {
 //     console.log("less then 1200");
 // }
 
-const loggedin = true
-const creaditcard = true
+// const loggedin = true
+// const creaditcard = true
 
-const googleloggedin = false
-const emailloggedin = true
+// const googleloggedin = false
+// const emailloggedin = true
 
-if( loggedin && creaditcard){
-    console.log("Allow to buy course");
-}
+// if( loggedin && creaditcard){
+//     console.log("Allow to buy course");
+// }
 
-if(googleloggedin || emailloggedin){
-    console.log("user logged in");
-}
+// if(googleloggedin || emailloggedin){
+//     console.log("user logged in");
+// }
+
+// ============ switch case ========================
+
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+// const month = 3
+
+// switch (month) {
+//     case 1:
+//         console.log("january");
+//         break;
+//     case 2:
+//         console.log("feb");
+//         break;
+//     case 3:
+//         console.log("march");
+//         break;
+//     case 4:
+//         console.log("april");
+//         break;
+
+//     default:
+//         console.log("default match");
+//         break;
+// }
+
+// falsy value 
+/* false, 0 , -0 , BigInt, "" , 0n, null, undefined, NaN */
+
+// truthy values
+/* "0", [], {}, " ", 'false', function(){}*/
