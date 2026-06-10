995511d116f1ac5d4f552e5d3d750d78839394da
Shivam16a <shivam123hjp@gmail.com>
2026-06-10 10:07:38 +0530

    while and do while

--- 01_basics/loop.js
+++ 01_basics/loop.js
@@ -26,3 +26,48 @@ for (let index = 0; index < muarr.length; index++) {
     const element = muarr[index];
     // console.log(element)
 }
+
+
+// break and continue
+
+// for (let index = 1; index <= 20; index++) {
+//     if(index == 5){
+//         console.log(`detected 5`);
+//         break;
+//     }
+//     console.log(`value of index is ${index}`); 
+// }
+
+
+for (let index = 1; index <= 20; index++) {
+    if(index == 5){
+        // console.log(`detected 5`);
+        continue;
+    }
+    // console.log(`value of index is ${index}`); 
+}
+
+// ============== while and do while =================
+
+let index = 0;
+
+while (index <= 10) {
+    // console.log(`Value of index is ${index}`);
+    index = index+2;
+}
+
+let myarr = ["motu", "patlu"]
+
+let arr = 0;
+while(arr<myarr.length){
+    // console.log(`Value is ${myarr[arr]}`);
+    arr = arr+1;
+}
+
+
+let score = 11;
+
+do {
+    // console.log(`score is ${score}`);
+    score++;
+} while (score<=10);
