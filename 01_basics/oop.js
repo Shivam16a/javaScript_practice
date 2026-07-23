bf41b42afa4da9e70b84367bad1fa78d275b5cd0
Shivam16a <shivam123hjp@gmail.com>
2026-07-23 14:49:53 +0530

    concept of creating object nad constructor

--- 01_basics/oop.js
+++ 01_basics/oop.js
@@ -9,6 +9,27 @@ const user = {
     }
 }
 
-console.log(user.username);
-console.log(user.getUserDetails());
+// console.log(user.username);
+// console.log(user.getUserDetails());
 // console.log(this);
+
+// const promissOne = new Promise()
+
+// "new" is constructor
+
+function User(username,logincount, isloggedin){
+    this.username = username;
+    this.logincount = logincount;
+    this.isloggedin = isloggedin;
+
+    return this
+}
+
+const userOne = new User("shivam",12,true);
+const userTwo = new User("Aman",6,false);
+console.log(userTwo.constructor);
+// console.log(userOne);
+
+// new-> create new empty Object 
+// call constructor function
+// inject argument
